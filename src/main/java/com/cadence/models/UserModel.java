2b185d6ff75867047c6137f00681d9a7b60ad869
Felp <wtffelp@gmail.com>
2026-05-11 20:02:41 -0300

    feat: add User model

---
+++ src/main/java/com/cadence/models/UserModel.java
@@ -0,0 +1,29 @@
+package com.cadence.models;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class UserModel {
+    private UUID id;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private Timestamp createdAt;
+
+    public UserModel() {}
+
+    public UUID getId(){ return id; }
+    public void setId(UUID id){ this.id = id; }
+
+    public String getUsername(){ return username; }
+    public void setUsername(String username){ this.username = username; }
+
+    public String getEmail(){ return email; }
+    public void setEmail(String email){ this.email = email; }
+
+    public String getPasswordHash(){ return passwordHash; }
+    public void setPasswordHash(String passwordHash){ this.passwordHash = passwordHash; }
+
+    public Timestamp getCreatedAt(){ return createdAt; }
+    public void setCreatedAt(Timestamp createdAt){ this.createdAt = createdAt; }
+}
