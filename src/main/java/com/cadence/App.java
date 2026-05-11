f6b890bf7d469485e0c37d3c33a2632472ea827b
Felp <wtffelp@gmail.com>
2026-05-11 19:48:37 -0300

    feat: javalin server with health check

--- src/main/java/com/cadence/App.java
+++ src/main/java/com/cadence/App.java
@@ -5,10 +5,21 @@ import java.sql.SQLException;
 
 import com.cadence.config.DatabaseConfig;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import io.javalin.Javalin;
+
+public class App {
+    public static void main( String[] args ){
+        Javalin javalin = Javalin.create(
+            config -> {
+                config.bundledPlugins.enableCors(cors -> {
+                    cors.addRule(rule -> {
+                        // rule.allowHost("https://cadence.vercel.app");
+                        rule.anyHost();
+                    });
+                });
+            }
+        ).start(7000);
+        javalin.get("/health", ctx -> ctx.result("OK"));
         try (Connection conn = DatabaseConfig.getDataSource().getConnection()){
             System.out.println("Conexão com o banco OK!");
         } catch (SQLException e) {
